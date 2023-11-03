1d1c769d3c6b05f1eacfa1b8d847e7c1c628c206
minyakonga <minyakonga@gmail.com>
2023-11-03 10:12:20 +0800

    test  pydb command line with pexpect and pytest

---
+++ test_main.py
@@ -0,0 +1,29 @@
+import pytest
+import pexpect
+
+
+@pytest.mark.asyncio
+async def test_pydb():
+    # 1st, open the pydb
+    client = pexpect.spawn('python main.py')
+    client.expect('db > ', timeout=3)
+
+    # 2nd, insert 2 rows
+    client.sendline('insert 1 cstack foo@bar.com')
+    client.expect('Executed.', timeout=3)
+    client.sendline('insert 2 bob bob@example.com')
+    client.expect('Executed.', timeout=3)
+
+    # 3rd, select all rows
+    client.sendline('select')
+    client.expect('(1, cstack, foo@bar.com)', timeout=3)
+    client.expect('(2, bob, bob@example.com)', timeout=3)
+
+    # 4th, insert a row with invalid field
+    client.sendline('insert foo bar 1')
+    client.expect('Syntax error. Could not parse statement.')
+
+    # 5th, .exit cmd
+    client.sendline('.exit')
+
+    client.close()
